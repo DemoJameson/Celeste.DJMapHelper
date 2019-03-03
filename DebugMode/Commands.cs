091544592ecc51f314275c7b9947314317130adc
DemoJameson <demojameson@gmail.com>
2019-03-03 23:59:09 +0800

    Added metadata command

---
+++ DebugMode/Commands.cs
@@ -0,0 +1,20 @@
+using Monocle;
+
+namespace Celeste.Mod.DJMapHelper.DebugMode {
+    public static class Commands {
+        [Command("metadata", "check metadata values")]
+        private static void CmdMetadata() {
+            Level scene = Engine.Scene as Level;
+            if (scene == null)
+                return;
+            
+            Engine.Commands.Log("color grade (" + scene.Session.ColorGrade + ")");
+            Engine.Commands.Log("base lighting (" + scene.BaseLightingAlpha + "), session lighting add(" +
+                                scene.Session.LightingAlphaAdd + "), current lighting (" +
+                                scene.Lighting.Alpha + ")");
+            Engine.Commands.Log("base bloom (" + (scene.Bloom.Base - scene.Session.BloomBaseAdd) +"), session bloom add(" +
+                                scene.Session.BloomBaseAdd + "), current bloom (" +
+                                scene.Bloom.Base + ")");
+        }
+    }
+}
