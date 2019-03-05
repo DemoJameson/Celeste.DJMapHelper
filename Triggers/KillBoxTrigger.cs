d4c6e14aeb6883e1efdb98ba781e2205eb66dfc2
DemoJameson <demojameson@gmail.com>
2019-03-05 13:03:05 +0800

    Fixed Bug: Madeline die even if she is invincible

--- Triggers/KillBoxTrigger.cs
+++ Triggers/KillBoxTrigger.cs
@@ -4,11 +4,14 @@ using Monocle;
 namespace Celeste.Mod.DJMapHelper.Triggers {
     [Tracked]
     public class KillBoxTrigger : Trigger {
-        public KillBoxTrigger(EntityData data, Vector2 offset) : base(data, offset) {}
+        public KillBoxTrigger(EntityData data, Vector2 offset) : base(data, offset) { }
 
         public override void OnEnter(Player player) {
             base.OnEnter(player);
-            player.Die((player.Position - Position).SafeNormalize());
+
+            if (!SaveData.Instance.Assists.Invincible) {
+                player.Die((player.Position - Position).SafeNormalize());
+            }
         }
     }
 }
