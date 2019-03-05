602ae5b2e6e5024d92ad0ac42aaba46881538b23
DemoJameson <demojameson@gmail.com>
2019-03-05 12:35:33 +0800

    Renames wind attack left to snowball left.

--- Ahorn/triggers/windAttackTriggerLeft.jl
+++ Ahorn/triggers/windAttackTriggerLeft.jl
@@ -5,7 +5,7 @@ using ..Ahorn, Maple
 @mapdef Trigger "DJMapHelper/windAttackTriggerLeft" WindAttackLeft(x::Integer, y::Integer, width::Integer=16, height::Integer=16)
 
 const placements = Ahorn.PlacementDict(
-    "WindAttack (Left) (DJMapHelper)" => Ahorn.EntityPlacement(
+    "Snowballs (Left) (DJMapHelper)" => Ahorn.EntityPlacement(
         WindAttackLeft,
         "rectangle"
     )
