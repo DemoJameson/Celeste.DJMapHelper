b5d47f308aa1e4e54d8e63003bc81dff6ea8e8ac
DemoJameson <demojameson@gmail.com>
2019-03-12 00:47:17 +0800

    Added "MaxDashes Trigger (Zero)"

--- Ahorn/triggers/maxDashesTrigger.jl
+++ Ahorn/triggers/maxDashesTrigger.jl
@@ -4,11 +4,19 @@ using ..Ahorn, Maple
 
 @mapdef Trigger "DJMapHelper/maxDashesTrigger" MaxDashes(x::Integer, y::Integer, width::Integer=16, height::Integer=16, dashes::String="One")
 const Dashes = String[
+    "Zero",
     "One",
     "Two"
 ]
 
 const placements = Ahorn.PlacementDict(
+    "Max Dashes (Zero) (DJMapHelper)" => Ahorn.EntityPlacement(
+        MaxDashes,
+        "rectangle",
+        Dict{String, Any}(
+            "dashes" => "Zero"
+        )
+    ),
     "Max Dashes (One) (DJMapHelper)" => Ahorn.EntityPlacement(
         MaxDashes,
         "rectangle",
