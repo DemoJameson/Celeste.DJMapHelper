3ec395ce28274aa5f976995aa79d2f625596a117
DemoJameson <demojameson@gmail.com>
2020-02-18 22:56:13 +0800

    新版 Ahorn Number 类型如果有初始值需要设置为小数

--- Ahorn/triggers/badelineProtectTrigger.jl
+++ Ahorn/triggers/badelineProtectTrigger.jl
@@ -2,7 +2,7 @@ module BadelineProtectTrigger
 
 using ..Ahorn, Maple
 
-@mapdef Trigger "DJMapHelper/badelineProtectTrigger" BadelineProtect(x::Integer, y::Integer, width::Integer=16, height::Integer=16, maxQuantity::Integer=3, radius::Integer=24, respwanTime::Number=8, rotationTime::Number=1.8, clockwise::Bool=true)
+@mapdef Trigger "DJMapHelper/badelineProtectTrigger" BadelineProtect(x::Integer, y::Integer, width::Integer=16, height::Integer=16, maxQuantity::Integer=3, radius::Integer=24, respwanTime::Number=8.0, rotationTime::Number=1.8, clockwise::Bool=true)
 
 const placements = Ahorn.PlacementDict(
     "Badeline Protect Trigger (DJMapHelper)" => Ahorn.EntityPlacement(
