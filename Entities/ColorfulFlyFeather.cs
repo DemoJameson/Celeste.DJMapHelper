26f5efee07fe0cbb04b0b126e70925a340c6f4f9
DemoJameson <demojameson@gmail.com>
2021-12-15 10:23:58 +0800

    feat: 不要写死羽毛原颜色改为从游戏中获取

--- Entities/ColorfulFlyFeather.cs
+++ Entities/ColorfulFlyFeather.cs
@@ -18,25 +18,25 @@ namespace Celeste.Mod.DJMapHelper.Entities {
 
         private static readonly FieldInfo StarFlyColorFieldInfo = typeof(Player).GetPrivateField("starFlyColor");
 
-        public static readonly Color OrigStarFlyColor = Calc.HexToColor("FFD65C");
-        private static readonly Color OrigFlyPowerHairColor = Calc.HexToColor("F2EB6D");
-        private static readonly Color OrigFlyPowerHairColor2 = Calc.HexToColor("FFF20F");
-        private static readonly Color OrigRespawnColor = Calc.HexToColor("FFDCA4");
-        private static readonly Color OrigRespawnColor2 = Calc.HexToColor("FFE95E");
+        public static Color OrigStarFlyColor = Calc.HexToColor("FFD65C");
+        private static Color origFlyPowerHairColor = Calc.HexToColor("F2EB6D");
+        private static Color origFlyPowerHairColor2 = Calc.HexToColor("FFF20F");
+        private static Color origRespawnColor = Calc.HexToColor("FFDCA4");
+        private static Color origRespawnColor2 = Calc.HexToColor("FFE95E");
 
         public static readonly Color BlueStarFlyColor = Calc.HexToColor("6DCFF6");
         public static readonly Color GreenStarFlyColor = Calc.HexToColor("66FF66");
         public static readonly Color RedStarFlyColor = Calc.HexToColor("F21E4F");
 
-        private static SpriteBank MySpriteBank;
+        private static SpriteBank mySpriteBank;
 
         private readonly Color starFlyColor;
         private FeatherColor color;
 
-        private Lazy<Color> FlyPowerHairColor => new Lazy<Color>(() => StarFlyColorAddHsv(12, -0.09, -0.06));
-        private Lazy<Color> FlyPowerHairColor2 => new Lazy<Color>(() => StarFlyColorAddHsv(12, -0.09));
-        private Lazy<Color> RespawnColor => new Lazy<Color>(() => StarFlyColorAddHsv(-8, -0.28));
-        private Lazy<Color> RespawnColor2 => new Lazy<Color>(() => StarFlyColorAddHsv(11));
+        private Lazy<Color> FlyPowerHairColor => new(() => StarFlyColorAddHsv(12, -0.09, -0.06));
+        private Lazy<Color> FlyPowerHairColor2 => new(() => StarFlyColorAddHsv(12, -0.09));
+        private Lazy<Color> RespawnColor => new(() => StarFlyColorAddHsv(-8, -0.28));
+        private Lazy<Color> RespawnColor2 => new(() => StarFlyColorAddHsv(11));
 
         // ReSharper disable once MemberCanBePrivate.Global
         public ColorfulFlyFeather(Vector2 position, bool shielded, bool singleUse, FeatherColor color) : base(position,
@@ -47,15 +47,15 @@ namespace Celeste.Mod.DJMapHelper.Entities {
             // ReSharper disable once SwitchStatementMissingSomeCases
             switch (color) {
                 case FeatherColor.Blue:
-                    MySpriteBank.CreateOn(sprite, "blueFlyFeather");
+                    mySpriteBank.CreateOn(sprite, "blueFlyFeather");
                     starFlyColor = BlueStarFlyColor;
                     break;
                 case FeatherColor.Green:
-                    MySpriteBank.CreateOn(sprite, "greenFlyFeather");
+                    mySpriteBank.CreateOn(sprite, "greenFlyFeather");
                     starFlyColor = GreenStarFlyColor;
                     break;
                 case FeatherColor.Red:
-                    MySpriteBank.CreateOn(sprite, "redFlyFeather");
+                    mySpriteBank.CreateOn(sprite, "redFlyFeather");
                     starFlyColor = RedStarFlyColor;
                     break;
                 default:
@@ -73,24 +73,39 @@ namespace Celeste.Mod.DJMapHelper.Entities {
         }
 
         public static void OnLoad() {
+            On.Celeste.Player.ctor += PlayerOnCtor;
             On.Celeste.FlyFeather.OnPlayer += FlyFeatherOnPlayer;
             On.Celeste.FlyFeather.Respawn += FlyFeatherOnRespawn;
         }
 
         public static void OnUnload() {
+            On.Celeste.Player.ctor -= PlayerOnCtor;
             On.Celeste.FlyFeather.OnPlayer -= FlyFeatherOnPlayer;
             On.Celeste.FlyFeather.Respawn -= FlyFeatherOnRespawn;
         }
 
         public static void OnLoadContent() {
-            MySpriteBank = new SpriteBank(GFX.Game, "Graphics/DJMapHelperSprites.xml");
+            mySpriteBank = new SpriteBank(GFX.Game, "Graphics/DJMapHelperSprites.xml");
+            InitOrigColors();
+        }
+
+        private static void InitOrigColors() {
+            origFlyPowerHairColor = P_Collect?.Color ?? Calc.HexToColor("F2EB6D");
+            origFlyPowerHairColor2 = P_Collect?.Color2 ?? Calc.HexToColor("FFF20F");
+            origRespawnColor = P_Respawn?.Color ?? Calc.HexToColor("FFDCA4");
+            origRespawnColor2 = P_Respawn?.Color2 ?? Calc.HexToColor("FFE95E");
+        }
+
+        private static void PlayerOnCtor(On.Celeste.Player.orig_ctor orig, Player self, Vector2 position, PlayerSpriteMode spriteMode) {
+            orig(self, position, spriteMode);
+            OrigStarFlyColor = (Color)StarFlyColorFieldInfo.GetValue(self);
         }
 
         private static void FlyFeatherOnPlayer(On.Celeste.FlyFeather.orig_OnPlayer orig, FlyFeather self,
             Player player) {
             Color starFlyColor;
             if (self.GetType() == typeof(ColorfulFlyFeather)) {
-                ColorfulFlyFeather colorfulFlyFeather = (ColorfulFlyFeather) self;
+                ColorfulFlyFeather colorfulFlyFeather = (ColorfulFlyFeather)self;
                 starFlyColor = colorfulFlyFeather.starFlyColor;
 
                 P_Collect.Color = colorfulFlyFeather.FlyPowerHairColor.Value;
@@ -102,28 +117,28 @@ namespace Celeste.Mod.DJMapHelper.Entities {
             } else {
                 starFlyColor = OrigStarFlyColor;
 
-                P_Collect.Color = OrigFlyPowerHairColor;
-                P_Collect.Color2 = OrigFlyPowerHairColor2;
-                P_Boost.Color = OrigFlyPowerHairColor;
-                P_Boost.Color2 = OrigFlyPowerHairColor2;
-                P_Flying.Color = OrigFlyPowerHairColor;
-                P_Flying.Color2 = OrigFlyPowerHairColor2;
+                P_Collect.Color = origFlyPowerHairColor;
+                P_Collect.Color2 = origFlyPowerHairColor2;
+                P_Boost.Color = origFlyPowerHairColor;
+                P_Boost.Color2 = origFlyPowerHairColor2;
+                P_Flying.Color = origFlyPowerHairColor;
+                P_Flying.Color2 = origFlyPowerHairColor2;
             }
 
-            StarFlyColorFieldInfo?.SetValue(player, starFlyColor);
+            StarFlyColorFieldInfo.SetValue(player, starFlyColor);
 
             orig(self, player);
         }
 
         private static void FlyFeatherOnRespawn(On.Celeste.FlyFeather.orig_Respawn orig, FlyFeather self) {
             if (self.GetType() == typeof(ColorfulFlyFeather)) {
-                ColorfulFlyFeather colorfulFlyFeather = (ColorfulFlyFeather) self;
+                ColorfulFlyFeather colorfulFlyFeather = (ColorfulFlyFeather)self;
 
                 P_Respawn.Color = colorfulFlyFeather.RespawnColor.Value;
                 P_Respawn.Color2 = colorfulFlyFeather.RespawnColor2.Value;
             } else {
-                P_Respawn.Color = OrigRespawnColor;
-                P_Respawn.Color2 = OrigRespawnColor2;
+                P_Respawn.Color = origRespawnColor;
+                P_Respawn.Color2 = origRespawnColor2;
             }
 
             orig(self);
